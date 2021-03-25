e51dc4b6702da4b3807f5f0550212a9377ab0cf9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2021-03-25 11:48:37 +0100

    setup.py ->setup.cfg

--- setup.py
+++ setup.py
@@ -1,32 +1,15 @@
-from distutils.core import setup
-setup(
-    name = "mxp",
-    packages = ["mxp"],
-    version = "0.1.3",
-    description = "qPCR fileformat reader",
-    author = "Florian FInkernagel",
-    author_email = "finkernagel@imt.uni-marburg.de",
-    url = "http://www.imt.uni-marburg.de",
-    keywords = ["qPCR", "MXP"],
-    install_requires=[
-                  'olefile',
-                  'pandas',
-                        ],
+import sys
 
-    classifiers = [
-        "Programming Language :: Python",
-        #"Programming Language :: Python :: 3",
-        "Development Status :: 4 - Beta",
-        "Environment :: Other Environment",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
-        "Operating System :: OS Independent",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-    long_description = """\
-Support code to read qPCR machine file formats,
-such as the .mxp file format created by MxPro.
+from pkg_resources import require, VersionConflict
+from setuptools import setup
 
-Currently reads files from 300 and 305er machines.
-"""
-)
+
+try:
+    require("setuptools>=38.3")
+except VersionConflict:
+    print("Error: version of setuptools is too old (<38.3)!")
+    sys.exit(1)
+
+
+if __name__ == "__main__":
+    setup()
